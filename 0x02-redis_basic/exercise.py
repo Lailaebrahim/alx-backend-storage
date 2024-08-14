76413c9402667cd9360e1bc6a12267857ee3c5c6
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-15 00:49:31 +0300

    Task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,14 +8,26 @@ store the input data in Redis using the random key and return the key.
   """
 import redis
 import uuid
+import functools
 from typing import Union
 
+
+def count_calls(method: callable) -> callable:
+  @functools.wraps(method)
+  def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+  return wrapper
+  
+  
 
 class Cache:
   def __init__(self):
     self._redis = redis.Redis()
     self._redis.flushdb()
-    
+   
+  @count_calls 
   def store(self, data: Union[str, bytes, int, float]) -> str:
     key = str(uuid.uuid4())
     self._redis.set(key, data)
