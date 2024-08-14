dea9f37428cc46d6eb9a5911c8262127cec80d95
lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-15 01:36:45 +0300

    Task 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -45,6 +45,7 @@ def call_history(method: Callable) -> Callable:
 
 def replay(fn: Callable) -> None:
     """Displays the call history of a Cache class' method.
+
     """
     if fn is None or not hasattr(fn, '__self__'):
         return
