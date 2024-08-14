06a33c1fac17bb414647415cdf1fc5338dfc68f1
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-08-13 18:35:58 -0700

    Task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,35 +1,29 @@
 #!/usr/bin/env python3
-"""
-Script that provides stats about Nginx logs stored in MongoDB
-"""
+""" Python script that provides some stats about Nginx logs stored in MongoDB:"""
 from pymongo import MongoClient
 
 
-def log_stats():
-    """Python script that provides some stats about Nginx logs stored in MongoDB"""
-    # Connect to MongoDB
+def print_logs_statistics(nginx_collection):
+    '''Prints stats about Nginx request logs.
+    '''
+    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('Request Methods:')
+    request_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in request_methods:
+        req_count = len(list(nginx_collection.find({'method': method})))
+        print('\tMethod {}: {}'.format(method, req_count))
+    status_checks_count = len(list(
+        nginx_collection.find({'method': 'GET', 'path': '/status'})
+    ))
+    print('{} status checks'.format(status_checks_count)
+
+
+def analyze_logs():
+    '''Provides some stats about Nginx logs stored in MongoDB.
+    '''
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    collection = db.nginx
-
-    # Get total number of logs
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
-
-    # Get stats for each method
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
-
-    # Get number of status check
-    status_check = collection.count_documents({
-        "method": "GET",
-        "path": "/status"
-    })
-    print(f"{status_check} status check")
+    print_logs_statistics(client.logs.nginx)
 
 
-if __name__ == "__main__":
-    log_stats()
+if __name__ == '__main__':
+    analyze_logs()
